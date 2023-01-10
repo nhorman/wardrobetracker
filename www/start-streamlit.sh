a37d4c05d0d8d7d6e796f4f45fc962e996d275ff
Neil Horman <neil.horman@privafy.com>
2023-01-10 10:34:38 -0500

    Fixup db initalization

--- www/start-streamlit.sh
+++ www/start-streamlit.sh
@@ -3,9 +3,21 @@
 
 check_init_db() {
 	local is_init
+	echo "CHECKING DB"
+	while true
+	do
+		is_init=$(echo "connect wardrobedb; Select * from inittable;" | mysql -u root --password=secret -h mysql 2>&1)
+		echo "$is_init"
+		echo "$is_init" | grep -q "ERROR 2002.*Can't connect to MySQL server"
+		if [ $? -eq 0 ]
+		then
+			echo "Waiting on db startup"
+			sleep 1
+			continue
+		fi
+		break
+	done
 
-	is_init=$(echo "connect wardrobedb; Select * from inittable;" | mysql -u root --password=secret -h mysql)
-	echo "$is_init"
 	echo "$is_init" | grep -q ERROR
 	if [ $? -eq 0 ]
 	then
@@ -15,7 +27,9 @@ check_init_db() {
 	fi
 }
 
-check_init_db
+echo "STARTING WARDROBE CONTAINER!!!!"
+
+check_init_db &
 
 streamlit run --server.port=8501 --server.address=0.0.0.0 /app/index.py
 
